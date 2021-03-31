eefef6d50db0d93721326fcd0c1ffc2e946eb895
Naran Torguud <ntorguud@gmail.com>
2021-03-31 05:04:16 +0000

    Create practice pages

---
+++ test/gsd.py
@@ -0,0 +1,14 @@
+num_a = int(input('Enter first positive integer: '))
+print()
+
+num_b = int(input('Enter second positive integer: '))
+print()
+
+while num_a != num_b:
+    if num_a > num_b:
+        num_a = num_a - num_b
+    else:
+        num_b = num_b - num_a
+
+print('Greatest common divisor is {}'.format(num_a))
+
---
+++ test/math_fraction.py
@@ -0,0 +1,6 @@
+user_num = (input()
+x = input()
+
+math.fractions = user_num / (3 * x)
+
+print(math.fractions)
